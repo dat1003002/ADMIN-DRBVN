ec17846e471844f0f16242b7ddde1c336d0ae7d7
dat1003002 <phanvandat10032002@gmail.com>
2025-05-15 10:55:05 +0700

    addcodexoamodel

--- Models/Product.cs
+++ Models/Product.cs
@@ -17,11 +17,9 @@ namespace AspnetCoreMvcFull.Models
     public string? tieuchuan { get; set; }
     public string? thucte { get; set; }
     public string? doday { get; set; }
-    public string? doday1 { get; set; }
-    public string? doday2 { get; set; }
     public string? soi1 { get; set; }
     public string? soi2 { get; set; }
-    public string? sodaycatduoc { get; set; } 
+    public string? sodaycatduoc { get; set; }
     public string? chieudaicatlon { get; set; }
     public string? chieudaicatnho { get; set; }
     public string? tocdomaydun { get; set; }
@@ -63,23 +61,6 @@ namespace AspnetCoreMvcFull.Models
     public string? trongluongloithepspinning { get; set; }
     public string? dodaycaosubo { get; set; }
     public string? dodaycaosuketdinh3t { get; set; }
-    public string? loaicaosu {  get; set; }
-    public string? loaicaosu1 { get; set; }
-    public string? loaicaosu2 { get; set; }
-    public string? loaikhuondun {  get; set; }
-    public string? loaikhuondun1 { get; set; }
-    public string? loaikhuondun2 { get; set; }
-    public string? tocdomotor { get; set; }
-    public string? tocdomotor1 { get; set; }
-    public string? tocdomotor2 { get; set; }
-    public string? chieudai {  get; set; }
-    public string? chieudai1 { get; set; }
-    public string? chieudai2 { get; set; }
-    public string? kho { get; set; }
-    public string? kho1 { get; set; }
-    public string? kho2 { get; set; }
-    public string? trongluong {  get; set; }
-    public string? trongluong1 { get; set; }
-    public string? trongluong2 { get; set; }
+    
   }
 }
--- Repository/QuyCachCSCTLRepository.cs
+++ Repository/QuyCachCSCTLRepository.cs
@@ -27,27 +27,7 @@ namespace AspnetCoreMvcFull.Repository
       {
         mahang = product.mahang,
         name = product.name,
-        loaicaosu = product.loaicaosu,
-        loaicaosu1 = product.loaicaosu1,
-        loaicaosu2 = product.loaicaosu2,
-        loaikhuondun = product.loaikhuondun,
-        loaikhuondun1 = product.loaikhuondun1,
-        loaikhuondun2 = product.loaikhuondun2,
-        tocdomotor = product.tocdomotor,
-        tocdomotor1 = product.tocdomotor1,
-        tocdomotor2 = product.tocdomotor2,
-        chieudai = product.chieudai,
-        chieudai1 = product.chieudai1,
-        chieudai2 = product.chieudai2,
-        kho = product.kho,
-        kho1 = product.kho1,
-        kho2 = product.kho2,
-        doday = product.doday,
-        doday1 = product.doday1,
-        doday2 = product.doday2,
-        trongluong = product.trongluong,
-        trongluong1 = product.trongluong1,
-        trongluong2 = product.trongluong2,
+
         CreatedAt = DateTime.Now,
         CategoryId = product.CategoryId
       };
@@ -81,27 +61,7 @@ namespace AspnetCoreMvcFull.Repository
         ProductId = product.ProductId,
         mahang = product.mahang,
         name = product.name,
-        loaicaosu = product.loaicaosu,
-        loaicaosu1 = product.loaicaosu1,
-        loaicaosu2 = product.loaicaosu2,
-        loaikhuondun = product.loaikhuondun,
-        loaikhuondun1 = product.loaikhuondun1,
-        loaikhuondun2 = product.loaikhuondun2,
-        tocdomotor = product.tocdomotor,
-        tocdomotor1 = product.tocdomotor1,
-        tocdomotor2 = product.tocdomotor2,
-        chieudai = product.chieudai,
-        chieudai1 = product.chieudai1,
-        chieudai2 = product.chieudai2,
-        kho = product.kho,
-        kho1 = product.kho1,
-        kho2 = product.kho2,
-        doday = product.doday,
-        doday1 = product.doday1,
-        doday2 = product.doday2,
-        trongluong = product.trongluong,
-        trongluong1 = product.trongluong1,
-        trongluong2 = product.trongluong2,
+        
         CreatedAt = product.CreatedAt,
         UpdatedAt = product.UpdatedAt
       };
@@ -116,27 +76,7 @@ namespace AspnetCoreMvcFull.Repository
           ProductId = p.ProductId,
           mahang = p.mahang,
           name = p.name,
-          loaicaosu = p.loaicaosu,
-          loaicaosu1 = p.loaicaosu1,
-          loaicaosu2 = p.loaicaosu2,
-          loaikhuondun = p.loaikhuondun,
-          loaikhuondun1 = p.loaikhuondun1,
-          loaikhuondun2 = p.loaikhuondun2,
-          tocdomotor = p.tocdomotor,
-          tocdomotor1 = p.tocdomotor1,
-          tocdomotor2 = p.tocdomotor2,
-          chieudai = p.chieudai,
-          chieudai1 = p.chieudai1,
-          chieudai2 = p.chieudai2,
-          kho = p.kho,
-          kho1 = p.kho1,
-          kho2 = p.kho2,
-          doday = p.doday,
-          doday1 = p.doday1,
-          doday2 = p.doday2,
-          trongluong = p.trongluong,
-          trongluong1 = p.trongluong1,
-          trongluong2 = p.trongluong2,
+         
           CreatedAt = p.CreatedAt,
           UpdatedAt = p.UpdatedAt
         }));
@@ -153,27 +93,7 @@ namespace AspnetCoreMvcFull.Repository
 
       product.mahang = quyCachCaoSuCTLDTO.mahang;
       product.name = quyCachCaoSuCTLDTO.name;
-      product.loaicaosu = quyCachCaoSuCTLDTO.loaicaosu;
-      product.loaicaosu1 = quyCachCaoSuCTLDTO.loaicaosu1;
-      product.loaicaosu2 = quyCachCaoSuCTLDTO.loaicaosu2;
-      product.loaikhuondun = quyCachCaoSuCTLDTO.loaikhuondun;
-      product.loaikhuondun1 = quyCachCaoSuCTLDTO.loaikhuondun1;
-      product.loaikhuondun2 = quyCachCaoSuCTLDTO.loaikhuondun2;
-      product.tocdomotor = quyCachCaoSuCTLDTO.tocdomotor;
-      product.tocdomotor1 = quyCachCaoSuCTLDTO.tocdomotor1;
-      product.tocdomotor2 = quyCachCaoSuCTLDTO.tocdomotor2;
-      product.chieudai = quyCachCaoSuCTLDTO.chieudai;
-      product.chieudai1 = quyCachCaoSuCTLDTO.chieudai1;
-      product.chieudai2 = quyCachCaoSuCTLDTO.chieudai2;
-      product.kho = quyCachCaoSuCTLDTO.kho;
-      product.kho1 = quyCachCaoSuCTLDTO.kho1;
-      product.kho2 = quyCachCaoSuCTLDTO.kho2;
-      product.doday = quyCachCaoSuCTLDTO.doday;
-      product.doday1 = quyCachCaoSuCTLDTO.doday1;
-      product.doday2 = quyCachCaoSuCTLDTO.doday2;
-      product.trongluong = quyCachCaoSuCTLDTO.trongluong;
-      product.trongluong1 = quyCachCaoSuCTLDTO.trongluong1;
-      product.trongluong2 = quyCachCaoSuCTLDTO.trongluong2;
+     
       product.UpdatedAt = DateTime.Now;
 
       _context.Products.Update(product);
